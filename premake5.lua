f83094483b32382df2808c6e96db42fcef725861
lmonfette <lmonfette@parilium-technologies.com>
2024-03-05 15:32:50 -0500

    modify the premake

--- premake5.lua
+++ premake5.lua
@@ -6,9 +6,23 @@ project "imgui"
     
     -- Add your source files here
     files {
-        "*.h",
-        "*.cpp",
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_opengl3.h",
+        "backends/imgui_impl_opengl3.cpp",
+        "imgui.h",
+        "imgui.cpp",
+        "imstb_truetype.h",
+        "imstb_textedit.h",
+        "imstb_rectpack.h",
+        "imgui_widgets.cpp",
+        "imgui_tables.cpp",
+        "imgui_internal.h",
+        "imgui_draw.cpp",
+        "imgui_demo.cpp",
     }
+
+    includedirs { ".", "../glfw/include" }
     
     filter "system:windows"
         architecture "x86_64"
